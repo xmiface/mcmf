3aa61047f325387e5cb3263d3ac88cc5191ddd73
smiface <smiface@mail.com>
2023-04-23 00:05:47 +0300

    fix

--- host-app/next.config.js
+++ host-app/next.config.js
@@ -1,17 +1,17 @@
-const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 
 module.exports = {
   webpack(config, options) {
     if (!options.isServer) {
       config.plugins.push(
         new NextFederationPlugin({
-          name: 'host',
+          name: "host",
           remotes: {
-            remote: 'remote@http://localhost:3001/remote.js',
-            remotee: 'remote@http://localhost:3002/remote.js',
+            remote: "remote@http://localhost:3001/remote.js",
+            remotee: "remotee@http://localhost:3002/remote.js",
           },
-          filename: 'static/chunks/remoteEntry.js',
-        }),
+          filename: "static/chunks/remoteEntry1.js",
+        })
       );
     }
 
